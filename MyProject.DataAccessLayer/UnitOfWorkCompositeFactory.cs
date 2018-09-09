259c2a8ff758d4ebdce7011ec8ee509ff5450c08
mural <mural@LAXMISAHASRA-LP>
2018-09-09 20:47:03 +0530

    Created

--- MyProject.DataAccessLayer/UnitOfWorkCompositeFactory.cs
+++ MyProject.DataAccessLayer/UnitOfWorkCompositeFactory.cs
@@ -14,20 +14,11 @@ namespace MyProject.DataAccessLayer
         {
             var sqlConnectionUnitOfWork = new SqlConnectionFactoryContext().BeginUnitOfWork(connectiontype, isolationLevel, null);
             var entityUnitOfWork = new EntityUnitOfWorkFactory().BeginUnitOfWork(connectiontype, isolationLevel, null);
-            //   var ecoReportEntityUnitOfWork = new EcoReportEntityUnitOfWorkFactory().BeginUnitOfWork(connectiontype, isolationLevel, null);
-            //  var mongoUnitOfWork = new MongoFactoryContext().BeginUnitOfWork(connectiontype, isolationLevel, null);
-            // The cache unit of work must be placed after the SQL unit of work. When the SQL transaction commit fails,
-            // an exception is thrown and the cache is not updated. The keys remain locked, but the locks have a
-            // relatively low time out value. After this time, the cache resumes normal operation for the affected keys.
-            //   var cacheUnitOfWork = new CacheUnitOfWorkFactory().BeginUnitOfWork(connectiontype, isolationLevel, null);
 
             return new UnitOfWorkComposite(new List<IUnitOfWork>
             {
                 sqlConnectionUnitOfWork,
                 entityUnitOfWork
-            //    ecoReportEntityUnitOfWork,
-             //   cacheUnitOfWork,
-              //  mongoUnitOfWork
             });
         }
     }
